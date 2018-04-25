b47086aa2a49b61de205c994071c85b3bf7f7604
asonnino <alberto.sonnino@ucl.ac.uk>
2018-04-25 16:42:34 +0100

    Read server type

--- server/server.cpp
+++ server/server.cpp
@@ -68,8 +68,8 @@ void session(tcp::socket sock)
       // set message
       ra_samp_request_header_t *p_msg0_full = NULL;
       p_msg0_full = (ra_samp_request_header_t*)
-        malloc(sizeof(ra_samp_request_header_t) +sizeof(uint32_t));
-      p_msg0_full->type = TYPE_RA_MSG0; // read_msg_.type(); // e.g., TYPE_RA_MSG0;
+        malloc(sizeof(ra_samp_request_header_t) + sizeof(uint32_t));
+      p_msg0_full->type = read_msg_.type(); // e.g., TYPE_RA_MSG0;
       p_msg0_full->size = sizeof(uint32_t);
       uint32_t extended_epid_group_id = 0;
       *(uint32_t*)((uint8_t*)p_msg0_full + sizeof(ra_samp_request_header_t)) = extended_epid_group_id;
