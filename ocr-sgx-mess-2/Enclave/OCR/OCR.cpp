57db4f2b70837e05eeefbff70d447402e543ce81
asonnino <alberto.sonnino@ucl.ac.uk>
2018-05-03 19:54:50 +0100

    Update

--- ocr-sgx-mess-2/Enclave/OCR/OCR.cpp
+++ ocr-sgx-mess-2/Enclave/OCR/OCR.cpp
@@ -10,71 +10,9 @@
 #include "../../lib/Letter.h"
 
 #include "ocr.h"
+#include "ocr_type.h"
 
 
-class Letter2 {
-private:
-	/**
-	 * The char ascii value of the character represented by this letter class.  Defaults to * if
-	 * the character is unknown (for instance, in the case of unscanned letters)
-	 */
-	char letter;
-
-	/**
-	 * A matrix containing
-	 */
-	vector< vector<int> > matrix;
-
-	/**
-	 * The x and y coordinates in the parent matrix of where the matrix was found
-	 */
-	int x;
-	int y;
-
-public:
-	//Initialize a new letter from a matrix
-	Letter2(vector< vector<int> > matrix);
-
-	//Getters and setters
-	char getLetter() const;
-	void setLetter(char letter);
-	const vector<vector<int> >& getMatrix() const;
-	void setMatrix(const vector<vector<int> >& matrix);
-
-	int getX() const;
-	void setX(int x);
-	int getY() const;
-	void setY(int y);
-
-	void exportLetter(int data[]);
-	static Letter2 importLetter(const int data[], const int rows, const int cols);
-};
-
-// getters and setters
-char Letter2::getLetter() const {
-	return letter;
-}
-void Letter2::setLetter(char letter) {
-	this->letter = letter;
-}
-int Letter2::getX() const {
-	return x;
-}
-void Letter2::setX(int x) {
-	this->x = x;
-}
-int Letter2::getY() const {
-	return y;
-}
-void Letter2::setY(int y) {
-	this->y = y;
-}
-const vector< vector<int> >& Letter2::getMatrix() const {
-	return matrix;
-}
-void Letter2::setMatrix(const vector< vector<int> >& matrix) {
-	this->matrix = matrix;
-}
 
 // initialize a new letter object
 Letter2::Letter2(vector< vector<int> > matrix) {
