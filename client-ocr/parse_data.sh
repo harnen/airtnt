62f5cfcd8cc00d9b0734082da1e517e23e376d6c
Michał Król <mharnen@gmail.com>
2018-05-08 09:24:51 +0100

    fix: :

--- client-ocr/parse_data.sh
+++ client-ocr/parse_data.sh
@@ -9,21 +9,38 @@ do
         ITER=`echo $line | cut -d ':' -f 2 | cut -d ' ' -f 1`
         LIMIT=`echo $line | cut -d ':' -f 3`
 #        echo Got iter: $ITER limit: $LIMIT
-        echo -en "$ITER\t"
     fi 
     echo $line | grep "Time connected" &> /dev/null
     if [ $? -eq 0 ]
     then   
         T_ALL=`echo $line | cut -d ']' -f 4 | cut -d ',' -f 1`
 #        echo Got T_ALL: $T_ALL
-        echo -en "$T_ALL\t"
     fi
+    echo $line | grep "Bytes received:" &> /dev/null
+    if [ $? -eq 0 ]
+    then   
+        BYTES_RECV=`echo $line | cut -d ':' -f 2`
+#        echo Got BYTES_RECV $BYTES_RECV
+    fi
+    
+    echo $line | grep "Bytes sent:" &> /dev/null
+    if [ $? -eq 0 ]
+    then   
+        BYTES_SENT=`echo $line | cut -d ':' -f 2`
+#        echo Got BYTES_SENT $BYTES_SENT
+    fi
+    
     echo $line | grep "Time spent in the enclave" &> /dev/null
     if [ $? -eq 0 ]
     then   
         T_ENC=`echo $line | cut -d ' ' -f 6`
 #        echo Got T_ENC: $T_ENC
-        echo  "$T_ENC"
+        echo -en "$ITER\t"
+        echo -en "$T_ALL\t"
+        echo -en "$T_ENC\t"
+        echo -en "$BYTES_SENT\t"
+        echo -en "$BYTES_RECV\t"
+        echo ""
     fi
     
 done < $1
